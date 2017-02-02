2ee21e5228590a8aa0cf47cf6a54dfc44cc4e1e1
yves_982 <lalanne.yves@gmx.fr>
2017-02-02 21:55:49 +0100

    Closing disclaimer from the cross button now toggles the top button

--- views/templates/js/movies/moviesActionList.js
+++ views/templates/js/movies/moviesActionList.js
@@ -11,13 +11,13 @@
         }
 
         function onMoviesReceived(data) {
-            
+
             $(data.movies).each(function(ind, elt){
                 if(ctx.editedMovies && ctx.editedMovies.indexOf(elt.id) !== -1) {
                     elt.mode = 'edit';
                 }
             });
-            
+
             ctx.data = data;
             var galleryContent = Handlebars.templates.moviesList(data);
             var childCnt = $('div.gallery').children().length;
@@ -27,7 +27,7 @@
             $('div.gallery').append(galleryContent);
             registerActions();
         }
-        
+
         function getHistoUrl(firstLetter) {
             var hashPos = window.location.href.lastIndexOf('#');
             var histoUrl = window.location.href;
@@ -36,7 +36,7 @@
             }
             return histoUrl;
         }
-        
+
         function pushHistoUrl(firstLetter) {
             if(window.history.pushState) {
                 window.history.pushState({ firstLetter: firstLetter }, '', '#' +firstLetter);
@@ -45,14 +45,14 @@
             }
         }
 
-        
+
         function onLetterClick(e) {
             var firstLetter = $(e.target).text().toLowerCase();
             loadFirstLetterBloc(firstLetter);
             pushHistoUrl(firstLetter);
             e.preventDefault();
         }
-        
+
         function loadFirstLetter(firstLetter) {
             var prefix = '';
             if(ctx.hasChangeRights) {
@@ -67,16 +67,16 @@
             $('#firstLetterForm').remove();
             $('.currentLetter').val(firstLetter);
         }
-        
-        
+
+
         function loadFirstLetterBloc(firstLetter, blocNum) {
             updateNbElementsMax();
-            
+
             var nbElementsMax = ctx.nbElementsMax;
             if(!blocNum) {
                 blocNum = 1;
             }
-            
+
             var prefix = '';
             if(ctx.hasChangeRights) {
                 prefix = '/admin';
@@ -92,7 +92,7 @@
             $('.currentLetter').val(firstLetter);
         }
 
-        
+
         function onFirstLetterClick(e){
             var firstLetter = $('#firstLetterInput').val().toLowerCase();
             loadFirstLetterBloc(firstLetter);
@@ -100,14 +100,14 @@
             e.preventDefault();
         }
 
-        
+
         function onFirstLetterFormSubmit(e) {
             var firstLetter = $('#firstLetterInput').val().toLowerCase();
             $('#firstLetterForm').attr('action', '#' +firstLetter);
             $('#firstLetterBtn').click();
             e.preventDefault();
         }
-        
+
         function onFirstLetterOtherClick(e) {
             if($('#firstLetterBtn').length == 0) {
                 var firstLetterForm = Handlebars.templates.moviesListFirstLetterOther();
@@ -119,25 +119,25 @@
             e.preventDefault();
         }
 
-        
+
         function addActionPanel() {
             var actionPanel = Handlebars.templates.actionPanel(ctx);
             $('div.main-panel').append(actionPanel);
         }
 
-        
+
         function addSearchBar() {
             var searchBar = Handlebars.templates.searchBar();
             $('div#row-nav1').append(searchBar);
         }
 
-        
+
         function addDisclaimerSwitch() {
             var disclaimerSwitch = Handlebars.templates.disclaimerSwitch();
             $('div#row-nav1').append(disclaimerSwitch);
         }
 
-        
+
         function handleUrlHash() {
             var hashPos = window.location.href.lastIndexOf('#');
             if(hashPos != -1) {
@@ -148,10 +148,10 @@
             } else {
                 loadFirstLetterBloc('');
             }
-                
+
         }
 
-        
+
         function addLoginIfNecessary() {
             if(!ctx.hasChangeRights) {
                 var loginCtx = { secureHost: ctx.host.replace('http:', 'https:') };
@@ -159,21 +159,21 @@
                 $('div.gallery').after(loginSection);
             }
         }
-        
+
         function addNavLinks() {
             var navContent = Handlebars.templates.moviesListNav();
             $('section[role=navigation]').first().append(navContent);
         }
-        
+
         function setLetterHandlers() {
             $('a.firstLetter').click(onLetterClick);
             $('#firstLetterOther').click(onFirstLetterOtherClick);
         }
-        
+
         function handleBackHistory(e) {
             handleUrlHash();
         }
-        
+
         function setBackHandler() {
             if(window.history.pushState) {
                 $(window).bind('popstate', handleBackHistory);
@@ -181,27 +181,28 @@
                 $(window).bind('hashchange', handleBackHistory);
             }
         }
-        
+
         function setCallbackEvents() {
             $('div.gallery').on('movieAdded', registerActions);
             $('div.gallery').on('moviesDeleted', registerActions);
             $('div.gallery').on('searchPerformed', registerActions);
             $('div.gallery').on('detailsClosed', registerActions);
         }
-        
+
         function closeDisclaimer() {
+            $('#row-nav1 > div > .btn-primary').click();
             $('#disclaimer-footer').toggleClass('hide', true);
         }
-        
+
         function toggleDisclaimer() {
             $('#disclaimer-footer').toggleClass('hide');
         }
-        
+
         function setDisclaimerHandlers() {
             $('#closeDisclaimer').click(closeDisclaimer);
             $('#disclaimerSwitch').change(toggleDisclaimer);
         }
-        
+
         function updatePagination() {
             var firstLetter = $('.currentLetter').val();
             updateNbElementsMax();
@@ -214,7 +215,7 @@
             }
             var data = {pages: pageNumbers, firstLetter: firstLetter, nbElementsMax: ctx.nbElementsMax };
             var pagination = Handlebars.templates.pagination(data);
-            
+
             if($('.pagination').length == 0) {
                 $('div.gallery').after(pagination);
             } else {
@@ -223,33 +224,33 @@
             setPaginationHandlers();
         }
 
-        
+
         function updateNbElementsMax() {
             ctx.nbElementsMax = Number.parseInt($('#nbElementsMax').val());
-            
+
             if(!ctx.nbElementsMax) {
                 ctx.nbElementsMax = 3;
             }
         }
 
-        
+
         function paginationClicked(e) {
             $('.search').val('');
             var blocNum = Number.parseInt($(e.target).text());
             var firstLetter = $('.currentLetter').val();
             loadFirstLetterBloc(firstLetter, blocNum);
         }
-        
+
         function setPaginationHandlers() {
             $('#nbElementsMax').change(updateNbElementsMax);
             $('ul.pagination li > a').click(paginationClicked);
         }
-        
+
         function moviesListInit() {
             addNavLinks();
             setLetterHandlers();
 
-            
+
             addActionPanel();
             addSearchBar();
             addDisclaimerSwitch();
@@ -260,8 +261,7 @@
             setCallbackEvents();
             setDisclaimerHandlers();
         }
-        
+
         moviesListInit();
     });
 }(jQuery.noConflict()));
-
